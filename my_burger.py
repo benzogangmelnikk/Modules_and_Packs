dbacdfbb048d96062131a79f47483c2ee57fc2ab
Cookie <eg.melnikk2@gmail.com>
2022-08-30 02:42:28 +0300

    Создан модуль my_burger для задания burger

---
+++ my_burger.py
@@ -0,0 +1,34 @@
+def add_bun():
+	print('А теперь добавим булочку')
+
+
+def add_cutlet():
+	print('А теперь добавим котлету')
+
+
+def add_cucumber():
+	print('А теперь добавим огурчик')
+
+
+def add_tomato():
+	print('А теперь добавим помидор')
+
+
+def add_mayonnaise():
+	print('А теперь добавим майонез')
+
+
+def add_cheese():
+	print('А теперь добавим сыр')
+
+
+def add_special_sauce():
+	print('А теперь добавим особый соус')
+
+
+def add_onion():
+	print('А теперь добавим лук')
+
+
+def add_salad():
+	print('А теперь добавим салат')
